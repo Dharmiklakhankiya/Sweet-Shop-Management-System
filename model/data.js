a7b633f4f49af83060ab5eece95b77998bebd6f7
Dharmik Lakhankiya <dharmiklakhankiya.com>
2025-07-17 18:20:45 +0530

    docs(data): embed sample sweet data in comments for easy testing

--- model/data.js
+++ model/data.js
@@ -13,4 +13,44 @@
 
 const data = [];
 
+/** * Sample data to populate the in-memory array.
+ * This data can be used for testing purposes.
+  data.push(
+    {
+      id: 1,
+      name: 'Gulab Jamun',
+      category: 'Desi',
+      price: 40,
+      quantity: 20
+    },
+    {
+      id: 2,
+      name: 'Kaju Katli',
+      category: 'Dry Fruit',
+      price: 60,
+      quantity: 15
+    },
+    {
+      id: 3,
+      name: 'Rasgulla',
+      category: 'Bengali',
+      price: 35,
+      quantity: 25
+    },
+    {
+      id: 4,
+      name: 'Ladoo',
+      category: 'Festival',
+      price: 25,
+      quantity: 30
+    },
+    {
+      id: 5,
+      name: 'Barfi',
+      category: 'Milk',
+      price: 30,
+      quantity: 18
+    }
+  );
+*/
 module.exports = data;
