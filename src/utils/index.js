4553bb0320962860eab45290fb4a49841e2b4d1f
Dharmik Lakhankiya <dharmiklakhankiya.com>
2025-07-17 16:37:28 +0530

    Setup utility index for centralized exports

---
+++ src/utils/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  addSweet: require('./addSweet'),
+  deleteSweet: require('./deleteSweet'),
+  viewSweets: require('./viewSweets'),
+  updateSweet: require('./updateSweet'),
+  searchSweet: require('./searchSweet'),
+  sortSweets: require('./sortSweet'),
+  restockSweet: require('./restockSweet'),
+  purchaseSweet: require('./purchaseSweet')
+};
