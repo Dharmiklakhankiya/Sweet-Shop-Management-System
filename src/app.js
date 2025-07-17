3b65c6e605fab78d2c651eac9d7d15b96e7b9b9a
Dharmik Lakhankiya <dharmiklakhankiya.com>
2025-07-17 17:29:57 +0530

    feat(api): add /deleteSweet/:id DELETE route to handle delete sweet

--- src/app.js
+++ src/app.js
@@ -37,5 +37,16 @@ app.post('/addSweet', (req, res) => {
   }
 });
 
+app.delete('/deleteSweet/:id', (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = deleteSweet(Number(id));
+    res.status(200).json(result);
+  }
+  catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
+
 
 module.exports = app;
